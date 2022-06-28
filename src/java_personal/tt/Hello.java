8e060866a6f955c2a5bd8498b62e7a2cabc7ca35
nehause <wlsgnlwo@gmail.com>
2022-06-28 09:24:56 +0900

    Hello world

---
+++ src/java_personal/tt/Hello.java
@@ -0,0 +1,10 @@
+package java_personal.tt;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		// 1000 Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
